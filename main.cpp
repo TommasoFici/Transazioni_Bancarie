a47f46602544d863f590acae99440240f2299d27
Tommaso <tommaso.fici@edu.unifi.it>
2024-09-03 17:47:19 +0200

    Aggiunta di classi per la tipologia di transazione

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+
+using namespace std;
+
+//Classe base per una transazione
+class Transazione {
+public:
+    Transazione(const std::string& data, float importo, const std::string& descrizione)
+    : data(data), importo(importo), descrizione(descrizione) {}
+
+    virtual ~Transazione() = default;
+
+    virtual std::string getType() const = 0;
+
+    virtual void display() const {
+        cout << "Data: " << data << " | Importo: " << importo << " € | Descrizione: " << descrizione << std::endl;
+    }
+
+    float getImporto() const {
+        return importo;
+    }
+
+protected:
+    std::string data;
+    float importo;
+    std::string descrizione;
+
+};
+
+//Classe derivata per rappresentare un entrata
+class Bonifico : public Transazione {
+public:
+    Bonifico(const std::string& data, float importo, const std::string& descrizione)
+    : Transazione(data, importo, descrizione) {}
+
+    std::string getType() const override {
+        return "Bonifico";
+    }
+};
+
+//Classe derivata per rappresentare un'uscita
+class Versamento : public Transazione {
+public:
+    Versamento(const std::string& data, float importo, const std::string& descrizione)
+    : Transazione(data, importo, descrizione) {}
+
+    std::string getType() const override {
+        return "Versamento";
+    }
+};
